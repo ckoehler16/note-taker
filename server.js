e0b17c4b808233692cca7cbd8407302e5fc23fee
Koehler <ckoehler16@gmail.com>
2022-05-18 21:09:17 -0400

    installed packages and rearranged folder/files

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const { notes } = require('./db/db.json');
+
+app.get('/api/notes', (req, res) => {
+    res.send
+})
+
+app.listen(PORT, () => {
+    console.log(`API server is now on port ${PORT}!`);
+})
