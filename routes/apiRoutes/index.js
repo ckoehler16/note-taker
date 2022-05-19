fadcbe373340ab0cf7a41b2e2c3a7dcc44e06375
Koehler <ckoehler16@gmail.com>
2022-05-18 22:07:02 -0400

    created routes for the api and html

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { validateNote, createNewNote } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+
+
+router.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+    // set the id based on what the next index of the array will be
+    req.body.id = notes.length.toSting();
+
+    // validate data in the body, send 400 error back if any data is incorrect
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note input is not properly formatted.');
+    }
+    else {
+        // add note to json file and notes array
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+const apiRoutes = require('./routes/apiRoutes/index');
+const htmlRoutes = require('./routes/htmlRoutes/index');
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const { notes } = require('./db/db.json');
+const exp = require('constants');
 
-app.get('/api/notes', (req, res) => {
-    res.send
-})
+// make public files (front end code) readily available (static resources)
+app.use(express.static('public'));
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server is now on port ${PORT}!`);
